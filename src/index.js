6a0285c8bc9b838317c542722b246aba4aceb2d1
Karol Bulwin <karolbulwin@gmail.com>
2019-04-25 10:56:32 +0200

    function instead of class

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ import "./index.css";
 class Square extends React.Component {
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
   }
 }
 
