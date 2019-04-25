97faf90d74156fe1a3db82281c86414a93fc1f6a
Karol Bulwin <karolbulwin@gmail.com>
2019-04-25 22:12:25 +0200

    changed to function , used map to create squares

--- src/index.js
+++ src/index.js
@@ -10,38 +10,16 @@ const Square = props => {
   );
 };
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+const Board = props => {
+  const sq = props.squares.map((square, index) => (
+    <Square
+      value={square}
+      key={[Symbol]}
+      onClick={() => props.onClick(index)}
+    />
+  ));
+  return sq;
+};
 
 class Game extends React.Component {
   constructor(props) {
