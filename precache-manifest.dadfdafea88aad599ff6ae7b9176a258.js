94943ccd6adeffa163418930f6a01858f6c7df7e
Karol Bulwin <karolbulwin@gmail.com>
2019-04-29 21:56:16 +0200

    Updates

---
+++ precache-manifest.dadfdafea88aad599ff6ae7b9176a258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0197f67140574278ad7a076d65857954",
+    "url": "/karolbulwin/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b4c7ae9cf893aa20b845",
+    "url": "/karolbulwin/Tic-tac-toe/static/css/main.a3e1cce7.chunk.css"
+  },
+  {
+    "revision": "d7b65b2bc53cfb76d4a7",
+    "url": "/karolbulwin/Tic-tac-toe/static/js/2.76dd4039.chunk.js"
+  },
+  {
+    "revision": "b4c7ae9cf893aa20b845",
+    "url": "/karolbulwin/Tic-tac-toe/static/js/main.8335b965.chunk.js"
+  },
+  {
+    "revision": "e717e9208aa84fe8df44",
+    "url": "/karolbulwin/Tic-tac-toe/static/js/runtime~main.7210a932.js"
+  }
+]);
