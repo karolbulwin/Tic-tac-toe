60f3a95695670023d664eca1637266ce063eec70
Karol Bulwin <karolbulwin@gmail.com>
2019-04-29 21:39:42 +0200

    sw

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import { TicTacToe } from "./components/TicTacToe";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<TicTacToe />, document.getElementById("root"));
+
+serviceWorker.unregister();
