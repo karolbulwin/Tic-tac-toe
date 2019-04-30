cf67225830f4883489d689d41971590d9f155c99
Karol Bulwin <karolbulwin@gmail.com>
2019-04-30 22:03:54 +0200

    unregister

---
+++ build/precache-manifest.5e82e242a04eb0291c3f4171a615edfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d23f9875a1d3c3eece23e6a191d46de",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "14228138c84baeb8c762",
+    "url": "/Tic-tac-toe/static/css/main.d6a31397.chunk.css"
+  },
+  {
+    "revision": "9b73165d3722a04452fd",
+    "url": "/Tic-tac-toe/static/js/2.08b12e5c.chunk.js"
+  },
+  {
+    "revision": "14228138c84baeb8c762",
+    "url": "/Tic-tac-toe/static/js/main.0b9e1da9.chunk.js"
+  },
+  {
+    "revision": "08ab2d31211831e52cd4",
+    "url": "/Tic-tac-toe/static/js/runtime~main.6066f4db.js"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/Tic-tac-toe/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/Tic-tac-toe/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/Tic-tac-toe/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/Tic-tac-toe/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/Tic-tac-toe/static/media/fa-solid-900.ff8d9f8a.ttf"
+  }
+]);
