a9bcdbf8cf0a3dc950ed2f5c6ee16a9a0ef34159
Karol Bulwin <karolbulwin@gmail.com>
2019-05-01 07:04:53 +0200

    unregister

--- build/precache-manifest.969fdabf9b3b43f9bfa894d7be5196e6.js
+++ build/precache-manifest.969fdabf9b3b43f9bfa894d7be5196e6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8fd655ab537e14ebc41f179314dc2892",
+    "revision": "567caf9946aa4a1c403ed7fa393e1130",
     "url": "/Tic-tac-toe/index.html"
   },
   {
-    "revision": "604ace50ca0ccbc807ac",
-    "url": "/Tic-tac-toe/static/css/main.3db3b2aa.chunk.css"
+    "revision": "04188d6b457e194d2478",
+    "url": "/Tic-tac-toe/static/css/main.e553067b.chunk.css"
   },
   {
     "revision": "d7b65b2bc53cfb76d4a7",
     "url": "/Tic-tac-toe/static/js/2.76dd4039.chunk.js"
   },
   {
-    "revision": "604ace50ca0ccbc807ac",
-    "url": "/Tic-tac-toe/static/js/main.d2691d00.chunk.js"
+    "revision": "04188d6b457e194d2478",
+    "url": "/Tic-tac-toe/static/js/main.3f03efcd.chunk.js"
   },
   {
     "revision": "08ab2d31211831e52cd4",
