40b5434e8a8e0b6d6ce03551412d5bd54306205c
Karol Bulwin <karolbulwin@gmail.com>
2019-05-08 20:58:25 +0200

    rebuild so gameStatus is a string

--- src/components/PlayAgain.js
+++ src/components/PlayAgain.js
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 const PlayAgain = props => (
 	<div className="game-done">
 		<div className="message">
-			{props.gameStatus !== null ? `The winner is ${props.gameStatus}` : "Draw"}
+			{props.gameStatus !== "Draw"
+				? `The winner is ${props.gameStatus}`
+				: "Draw"}
 		</div>
 		<button onClick={props.onClick}>Play Again</button>
 	</div>
