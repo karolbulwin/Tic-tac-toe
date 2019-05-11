ba6556688d8a556ab57c005192a86b33d14f031d
Karol Bulwin <karolbulwin@gmail.com>
2019-05-11 21:07:05 +0200

    export default

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Square from "./Square";
-export const Board = props => {
+
+const Board = props => {
 	const sq = props.squares.map((square, index) => (
 		<Square value={square} key={index} onClick={() => props.onClick(index)} />
 	));
 	return sq;
 };
+
+export default Board;
