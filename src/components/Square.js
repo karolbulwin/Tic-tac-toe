31f32b3f8e7dd42d26927042e87b18fcf621b172
Karol Bulwin <karolbulwin@gmail.com>
2019-05-11 20:41:52 +0200

    export default

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,10 @@
 import React from "react";
-export const Square = props => {
+const Square = props => {
 	return (
 		<button className="square" onClick={props.onClick}>
 			{props.value}
 		</button>
 	);
 };
+
+export default Square;
