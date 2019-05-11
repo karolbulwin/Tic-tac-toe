8650399fb134a2668c1f701b317444f9a5c4c338
Karol Bulwin <karolbulwin@gmail.com>
2019-05-11 20:42:03 +0200

    import default

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Square } from "./Square";
+import Square from "./Square";
 export const Board = props => {
 	const sq = props.squares.map((square, index) => (
 		<Square value={square} key={index} onClick={() => props.onClick(index)} />
