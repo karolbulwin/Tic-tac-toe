78d0130a0aac9aa0d4c10cdc1078d5f241c7fbf7
Karol Bulwin <karolbulwin@gmail.com>
2019-05-13 21:44:54 +0200

    added 2 tests

--- src/components/TicTacToe.Enzyme.test.js
+++ src/components/TicTacToe.Enzyme.test.js
@@ -8,6 +8,19 @@ function renderTicTacToe() {
 
 it("should render GameMenu", () => {
 	const wrapper = renderTicTacToe();
-	// console.log(wrapper.debug());
 	expect(wrapper.find("GameMenu").length).toBe(1);
+});
+
+it("should render Game for One Player without GameMenu", () => {
+	const wrapper = renderTicTacToe();
+	wrapper.find("button[name='one-player']").simulate("click");
+	expect(wrapper.find("Game[gameType='one-player']").length).toBe(1);
+	expect(wrapper.find("GameMenu").length).toBe(0);
+});
+
+it("should render Game for Two Players without GameMenu", () => {
+	const wrapper = renderTicTacToe();
+	wrapper.find("button[name='two-players']").simulate("click");
+	expect(wrapper.find("Game[gameType='two-players']").length).toBe(1);
+	expect(wrapper.find("GameMenu").length).toBe(0);
 });
