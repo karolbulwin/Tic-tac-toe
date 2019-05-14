d6cfe3d7049324123f23c15d77afb45e68d8e63e
Karol Bulwin <karolbulwin@gmail.com>
2019-05-14 16:49:03 +0200

    upgraded test

--- src/components/TicTacToe.Enzyme.test.js
+++ src/components/TicTacToe.Enzyme.test.js
@@ -42,17 +42,37 @@ it("new Board should render, Squares should be empty", () => {
 	wrapper.find("Square").forEach(square => expect(square.text()).toBe(""));
 });
 
-it("should go back two moves from one click on a left arrow from 'Go to move #5'to 'Go to move #3' ", () => {
+it("should go back two moves from one click on a left arrow, odd moves should be disabled", () => {
 	const wrapper = renderTicTacToe();
 	wrapper.find("button[name='one-player']").simulate("click");
 	const squares = wrapper.find("Square");
 	squares.at(0).simulate("click");
-	squares.at(1).simulate("click");
 	squares.at(4).simulate("click");
-	squares.at(2).simulate("click");
 	squares.at(8).simulate("click");
+	squares.at(1).simulate("click");
+	squares.at(7).simulate("click");
+	squares.at(6).simulate("click");
+	squares.at(2).simulate("click");
+	squares.at(5).simulate("click");
 
-	expect(wrapper.find(".dropbtn").text()).toBe("Move #5");
+	const buttons = wrapper.find("MovesControlMenu ul button");
+	expect(buttons.at(0).props()["disabled"]).toBe(false);
+	expect(buttons.at(1).props()["disabled"]).toBe(true);
+	expect(buttons.at(2).props()["disabled"]).toBe(false);
+	expect(buttons.at(3).props()["disabled"]).toBe(true);
+	expect(buttons.at(4).props()["disabled"]).toBe(false);
+	expect(buttons.at(5).props()["disabled"]).toBe(true);
+	expect(buttons.at(6).props()["disabled"]).toBe(false);
+	expect(buttons.at(7).props()["disabled"]).toBe(true);
+	expect(buttons.at(8).props()["disabled"]).toBe(false);
+
+	expect(wrapper.find(".dropbtn").text()).toBe("Move #8");
 	wrapper.find(".arrow-left").simulate("click");
-	expect(wrapper.find(".dropbtn").text()).toBe("Move #3");
+	expect(wrapper.find(".dropbtn").text()).toBe("Move #6");
+	wrapper.find(".arrow-left").simulate("click");
+	expect(wrapper.find(".dropbtn").text()).toBe("Move #4");
+	wrapper.find(".arrow-left").simulate("click");
+	expect(wrapper.find(".dropbtn").text()).toBe("Move #2");
+	wrapper.find(".arrow-left").simulate("click");
+	expect(wrapper.find(".dropbtn").text()).toBe("Go to start");
 });
